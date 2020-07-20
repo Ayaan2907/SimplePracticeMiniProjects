e27600a60be15daf6d2c4e75b3e38e7cbc0f8993
ayaanansari7921 <ayaanansari7921@outlook.com>
2020-07-20 23:18:59 +0530

    worked on fetchApi for getting truth text value of numbers

--- numbersFact/srcipt.js
+++ numbersFact/srcipt.js
@@ -1,23 +1,30 @@
-console.log("hello");
-
 // declare variables
+
 let truth = document.querySelector('#truth');
 let truthText = document.querySelector('#truthText');
 let inputNumber = document.querySelector('#inputNumber');
 
-// add input event listener, and call Fetch Api function
-inputNumber.addEventListener('input', getTruthFetch);
 
-// generating fumction for fetccccccccccch api
+// add input event listener, and call Fetch Api function
+// generating fumction for fetch api
+
+inputNumber.addEventListener('input', getTruthFetch);
 function getTruthFetch() {
       let num = inputNumber.value;
-      console.log(num)
-
+      console.log(num);
+      if(num != ''){ 
+            fetch(`http://numbersapi.com/${num}`) 
+            .then(response => response.text())
+            .then(Truth => { 
+              truth.style.display = 'block';
+              truthText.innerText = Truth;
+            })
+            .catch(err => console.log(err)); 
+          }
 };
 
+
 // add input event listener, and call ajx function
-inputNumber.addEventListener('input', getTruthAjx);      
-function getTruthAjx() {
-      let num = inputNumber.value;
-      console.log(num + 1)
-}; 
+// generating fumction for Ajx
+
+// inputNumber.addEventListener('input', getTruthAjx);      
